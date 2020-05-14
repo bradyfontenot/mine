38fe76091d5ef23e8135c854cabe3524ad0f348a
Brady Fontenot <bfont52@gmail.com>
2020-05-14 18:33:21 -0500

    Update error message

--- lib/mine_web/controllers/saved_controller.ex
+++ lib/mine_web/controllers/saved_controller.ex
@@ -25,10 +25,10 @@ defmodule MineWeb.SavedController do
 
   def index(conn, params) do
     case Reddit.get_user_saves(params["code"]) do
-      {:error, "invalid_grant"} ->
+      {:error, _} ->
         {:ok,
          conn
-         |> put_flash(:error, "Shit went wrong bro!")
+         |> put_flash(:error, "Try again please. Reddit seems to be a little moody.")
          |> redirect(to: "/")}
 
       entries ->
