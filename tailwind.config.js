1d9683759028758d93fffe383144b44880370e46
Brady Fontenot <bfont52@gmail.com>
2020-05-14 18:12:33 -0500

    Reset to default

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,9 @@
 module.exports = {
   purge: [],
   theme: {
-    colors:{
-      transparent: 'transparent',
-      peach1: '#FFEBCF',
-      peach2: '#F1D1B5',
-      pink1: '#F0B7A4',
-      pink2: '#F18C8E',
-      gblue: '#305F72'
-    }
-    extend: {},
+    colors:{},
+    extend: {}
   },
   // variants: {},
   // plugins: [],
 }
-
-
-
-
-FFEBCF - F1D1B5 - F0B7A4 - F18C8E - 305F72
