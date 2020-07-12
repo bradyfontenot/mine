fa02aaea4f6174b1a2bf70d7ae7bc8492ba26fc0
Brady Fontenot <bfont52@gmail.com>
2020-07-12 11:28:46 -0500

    Fix syntax error

--- config/prod.exs
+++ config/prod.exs
@@ -17,7 +17,7 @@ secret_key_base =
     """
 
 config :mine, MineWeb.Endpoint,
-  http: [port: 4000}],
+  http: [port: 4000],
   url: [scheme: "https", host: "mineapp-env.eba-nxjz2ubz.us-east-2.elasticbeanstalk.com", port: 4000],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json",
