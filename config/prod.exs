4cbcf134438c16e439d99bd70bb60ea9f75e2bbf
Brady Fontenot <bfont52@gmail.com>
2020-07-13 12:03:20 -0500

    Update Endpoint config. * remove unecessary comments

--- config/prod.exs
+++ config/prod.exs
@@ -18,28 +18,16 @@ secret_key_base =
 
 config :mine, MineWeb.Endpoint,
   server: true,
-  http: [port: 4000],
-  # url: [scheme: "http", host: "mineapp-env.eba-nxjz2ubz.us-east-2.elasticbeanstalk.com/", port: {:system, "PORT"}],
-  url: [host: "mineapp-env.eba-nxjz2ubz.us-east-2.elasticbeanstalk.com/"],
-  force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  url: [host: "mineapp-env.eba-nxjz2ubz.us-east-2.elasticbeanstalk.com", port: 3000],
+  # force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: secret_key_base,
-  transport_options: [socket_opts: [:inet6]]
+  http: [
+    transport_options: [socket_opts: [:inet6]],
+    port: 3000]
 
 # Do not print debug messages in production
 config :logger, level: :info
-
-# database_url =
-#   System.get_env("DATABASE_URL") ||
-#     raise """
-#     environment variable DATABASE_URL is missing.
-#     For example: ecto://USER:PASS@HOST/DATABASE
-#     """
-
-# config :mine, Mine.Repo,
-#   ssl: true,
-#   url: database_url,
-#   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
 
 reddit_api_key =
   System.get_env("REDDIT_API_KEY") ||
@@ -62,41 +50,3 @@ reddit_redirect_uri =
 config :mine, reddit_api_key: reddit_api_key
 config :mine, reddit_client_id: reddit_client_id
 config :mine, reddit_redirect_uri: reddit_redirect_uri
-
-# ## SSL Support
-#
-# To get SSL working, you will need to add the `https` key
-# to the previous section and set your `:url` port to 443:
-#
-#     config :mine, MineWeb.Endpoint,
-#       ...
-#       url: [host: "example.com", port: 443],
-#       https: [
-#         port: 443,
-#         cipher_suite: :strong,
-#         keyfile: System.get_env("SOME_APP_SSL_KEY_PATH"),
-#         certfile: System.get_env("SOME_APP_SSL_CERT_PATH"),
-#         transport_options: [socket_opts: [:inet6]]
-#       ]
-#
-# The `cipher_suite` is set to `:strong` to support only the
-# latest and more secure SSL ciphers. This means old browsers
-# and clients may not be supported. You can set it to
-# `:compatible` for wider support.
-#
-# `:keyfile` and `:certfile` expect an absolute path to the key
-# and cert in disk or a relative path inside priv, for example
-# "priv/ssl/server.key". For all supported SSL configuration
-# options, see https://hexdocs.pm/plug/Plug.SSL.html#configure/1
-#
-# We also recommend setting `force_ssl` in your endpoint, ensuring
-# no data is ever sent via http, always redirecting to https:
-#
-#     config :mine, MineWeb.Endpoint,
-#       force_ssl: [hsts: true]
-#
-# Check `Plug.SSL` for all available options in `force_ssl`.
-
-# Finally import the config/prod.secret.exs which loads secrets
-# and configuration from environment variables.
-# import_config "prod.secret.exs"
