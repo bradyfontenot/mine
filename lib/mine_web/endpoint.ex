cc754571959ccfc047e3e3ee78b1ee768616f9c1
Brady Fontenot <bfont52@gmail.com>
2020-07-17 11:02:40 -0500

    Remove ecto

--- lib/mine_web/endpoint.ex
+++ lib/mine_web/endpoint.ex
@@ -32,7 +32,6 @@ defmodule MineWeb.Endpoint do
     socket "/phoenix/live_reload/socket", Phoenix.LiveReloader.Socket
     plug Phoenix.LiveReloader
     plug Phoenix.CodeReloader
-    plug Phoenix.Ecto.CheckRepoStatus, otp_app: :mine
   end
 
   plug Phoenix.LiveDashboard.RequestLogger,
